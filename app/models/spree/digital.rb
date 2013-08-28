59c23bcf91133b702ae66101678f61d6a9cb4409
Grant Warman <grant.warman@gmail.com>
2013-08-28 18:22:49 -0400

    added s3 support to digital

--- app/models/spree/digital.rb
+++ app/models/spree/digital.rb
@@ -4,6 +4,18 @@ module Spree
     has_many :digital_links, :dependent => :destroy
 
     has_attached_file :attachment, :path => ":rails_root/private/digitals/:id/:basename.:extension"
+
+    include Spree::Core::S3Support
+    supports_s3 :attachment
+
+    #TODO: merge this option into supports_s3 in Spree::Core::S3Support
+    if Spree::Config[:use_s3]
+      attachment_definitions[:attachment][:s3_permissions] = :private
+    end
+
+
+
+    
   
     # TODO: Limit the attachment to one single file. Paperclip supports many by default :/
   end
