0d997eb4f5fb251535ce54498df7d4b781d0366c
Grant Warman <grant.warman@gmail.com>
2013-08-29 15:08:27 -0400

    Added shipping method to proper folder

---
+++ app/models/spree/calculator/shipping/digital_delivery.rb
@@ -0,0 +1,19 @@
+# https://github.com/spree/spree/issues/1439
+require_dependency 'spree/calculator'
+
+module Spree
+    module Calculator::Shipping
+      class DigitalDelivery < FlatRate
+        def self.description
+          I18n.t(:digital_delivery)
+        end
+    
+        def compute(object=nil)
+          self.preferred_amount
+        end
+    
+        def available?(order)
+          order.digital?
+        end
+  end
+end
